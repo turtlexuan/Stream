19dd5fb772646711fc8930288653f8422979c6c3
turtlexuan <turtlexuan@gmail.com>
2017-05-11 12:59:07 +0800

    1. Create Project 2. Pod install

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by 劉仲軒 on 2017/5/11.
+//  Copyright © 2017年 劉仲軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
